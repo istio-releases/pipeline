f180f9149c31019bb4118b98bb1ddb51b9d8d0a6
Charles Xu <chx@google.com>
2017-11-13 17:50:05 -0800

    Green build tracking and qualification script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # daily-release
+
+This repo tracks latest daily release version. Release qualification is triggered by creating a pull request against master. See istio/test-infra/toolbox/githubctl for the tool that does so. 
---
+++ qualify.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright 2017 Istio Authors
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+#######################################
+# Presubmit script triggered by Prow. #
+#######################################
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+# exports $HUB and $TAG
+source greenBuild.VERSION
+
+git clone https://github.com/istio/istio.git
+cd istio
+./tests/e2e.sh ${E2E_ARGS[@]:-} "$@" \
+  --mixer_tag "${TAG}"\
+  --mixer_hub "${HUB}"\
+  --pilot_tag "${TAG}"\
+  --pilot_hub "${HUB}"\
+  --ca_tag "${TAG}"\
+  --ca_hub "${HUB}"\
+  --istioctl_url "https://storage.googleapis.com/istio-artifacts/pilot/${TAG}/artifacts/istioctl"
