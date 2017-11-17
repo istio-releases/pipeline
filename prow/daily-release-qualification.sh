d5cbd9c7d38bee231219a10fc6220aab7af609a8
Charles Xu <chx@google.com>
2017-11-16 17:14:05 -0800

    check if has access to pull image

--- prow/daily-release-qualification.sh
+++ prow/daily-release-qualification.sh
@@ -30,13 +30,15 @@ set -x
 source greenBuild.VERSION
 echo "Using artifacts from HUB=${HUB} TAG=${TAG}"
 
-git clone https://github.com/istio/istio.git
-cd istio
-./tests/e2e.sh ${E2E_ARGS[@]:-} "$@" \
-  --mixer_tag "${TAG}"\
-  --mixer_hub "${HUB}"\
-  --pilot_tag "${TAG}"\
-  --pilot_hub "${HUB}"\
-  --ca_tag "${TAG}"\
-  --ca_hub "${HUB}"\
-  --istioctl_url "https://storage.googleapis.com/delco-experimental/builds/master/${TAG}"
+gcloud docker -- pull gcr.io/delco-experimental/builds/master/0.4.1-20171115-all/istio-ca:0.4.1-20171115-all
+
+# git clone https://github.com/istio/istio.git
+# cd istio
+# ./tests/e2e.sh ${E2E_ARGS[@]:-} "$@" \
+#   --mixer_tag "${TAG}"\
+#   --mixer_hub "${HUB}"\
+#   --pilot_tag "${TAG}"\
+#   --pilot_hub "${HUB}"\
+#   --ca_tag "${TAG}"\
+#   --ca_hub "${HUB}"\
+#   --istioctl_url "https://storage.googleapis.com/delco-experimental/builds/master/${TAG}"
