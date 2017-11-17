27203a0fe6f0387f84bb609e685d5ea899bd6652
Charles Xu <chx@google.com>
2017-11-16 16:31:10 -0800

    use matt's hub

--- prow/daily-release-qualification.sh
+++ prow/daily-release-qualification.sh
@@ -39,4 +39,4 @@ cd istio
   --pilot_hub "${HUB}"\
   --ca_tag "${TAG}"\
   --ca_hub "${HUB}"\
-  --istioctl_url "https://storage.googleapis.com/delco-experimental/builds/master/0.4.1-20171115-all"
+  --istioctl_url "https://storage.googleapis.com/delco-experimental/builds/master/${TAG}"
