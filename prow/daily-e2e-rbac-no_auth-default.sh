db9f1ca30dd25f93c92c4de8a336953161defada
Charles Xu <chx@google.com>
2017-11-28 20:45:42 +0000

    Another job to use default proxy

---
+++ prow/daily-e2e-rbac-no_auth-default.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright 2017 Istio Authors
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+#######################################
+# Presubmit script triggered by Prow. #
+#######################################
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+# exports $HUB, $TAG, and $GCS_PATH
+source greenBuild.VERSION
+echo "Using artifacts from HUB=${HUB} TAG=${TAG} GCS_PATH=${GCS_PATH}"
+
+git clone https://github.com/istio/istio.git
+cd istio
+./tests/e2e.sh "$@" \
+  --default_proxy
+  --mixer_tag "${TAG}"\
+  --mixer_hub "${HUB}"\
+  --pilot_tag "${TAG}"\
+  --pilot_hub "${HUB}"\
+  --ca_tag "${TAG}"\
+  --ca_hub "${HUB}"\
+  --istioctl_url "${GCS_PATH}"
