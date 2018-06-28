d46d7c94a9a665649d4ff8e3301da9f973ee364d
Charles Xu <chx@google.com>
2018-06-28 16:08:04 -0700

    Generate JUnit Report for New Jobs (#727)

--- prow/e2e-suite.sh
+++ prow/e2e-suite.sh
@@ -101,7 +101,7 @@ E2E_ARGS=(
   --test_logs_path="${ARTIFACTS_DIR}"
 )
 
-E2E_TARGET=${E2E_TARGET:-e2e_all_junit_report}
+E2E_TARGET=${E2E_TARGET:-e2e_all}
 time E2E_ARGS="${E2E_ARGS[@]}" EXTRA_E2E_ARGS="$@" \
   JUNIT_E2E_XML="${ARTIFACTS_DIR}/junit_daily-release.xml" \
-  make ${E2E_TARGET}
+  make with_junit_report TARGET=${E2E_TARGET}
