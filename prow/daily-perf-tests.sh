ef04fd9ae2a036301d68ffca8615a427eb6b2577
Krishna Pagadala <rkrishnap@google.com>
2018-07-11 18:40:35 -0700

    fix daily perf tests (#749)
    * fix daily perf tests
    
    * fix daily perf tests
    
    * debuggin
    
    * debuggin
    
    * debuggin

--- prow/daily-perf-tests.sh
+++ prow/daily-perf-tests.sh
@@ -19,74 +19,59 @@ set -e
 # Check unset variables
 set -u
 # Print commands
-set -x
+#set -x
 
 BASE_DIR=$PWD
-
-function cleanup() {
-  # Mason cleanup
-  mason_cleanup
-  cat "${FILE_LOG}"
-  cat "${PERF_LOG}"
-}
-
-source greenBuild.VERSION
-# Exports $HUB, $TAG
-echo "Using artifacts from HUB=${HUB} TAG=${TAG}"
-
-# Check https://github.com/istio/test-infra/blob/master/boskos/configs.yaml
-# for existing resources types
-RESOURCE_TYPE="${RESOURCE_TYPE:-gke-perf-preset}"
-OWNER='e2e-daily'
-INFO_PATH="$(mktemp /tmp/XXXXX.boskos.info)"
-FILE_LOG="$(mktemp /tmp/XXXXX.boskos.log)"
 
 # Artifact dir is hardcoded in Prow - boostrap to be in first repo checked out
 ARTIFACTS_DIR="${GOPATH}/src/github.com/istio-releases/daily-release/_artifacts"
 
-ISTIO_SHA=`curl $ISTIO_REL_URL/manifest.xml | grep -E "name=\"(([a-z]|-)*)/istio\"" | cut -f 6 -d \"`
-[[ -z "${ISTIO_SHA}"  ]] && echo "error need to test with specific SHA" && exit 1
+function cleanup() {
+  pwd; ls
+  cat qps*
+  sleep 15m
 
-# Checkout istio at the greenbuild
-mkdir -p ${GOPATH}/src/istio.io
-pushd ${GOPATH}/src/istio.io
-git clone -n https://github.com/istio/istio.git
-pushd istio
-git checkout $ISTIO_SHA
+  # Mason cleanup
+  mason_cleanup
+  cat "${FILE_LOG}"
+}
 
-source "prow/mason_lib.sh"
-source "prow/cluster_lib.sh"
+function get_istio_code() {
+  source greenBuild.VERSION
+  # Exports $HUB, $TAG
+  echo "Using artifacts from HUB=${HUB} TAG=${TAG}"
 
-trap cleanup EXIT
-
-# use uploaded yaml artifacts rather than the ones generated locally
-DAILY_BUILD=istio-$(echo ${ISTIO_REL_URL} | cut -d '/' -f 6)
-LINUX_DIST_URL=${ISTIO_REL_URL}/${DAILY_BUILD}-linux.tar.gz
-DEB_URL=${ISTIO_REL_URL}/deb
-#disable ISTIO_REL_URL
-unset ISTIO_REL_URL
-
-echo core account is
-gcloud config get-value core/account
-
-wget -q $LINUX_DIST_URL
-tar -xzf ${DAILY_BUILD}-linux.tar.gz
-cp -R ${DAILY_BUILD}/install/* install/
-
-wget -q https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
-tar -xvf helm-v2.9.1-linux-amd64.tar.gz
-linux-amd64/helm template --set sidecarInjectorWebhook.enabled=false \
-   --set global.proxy.image=proxy --namespace istio-system \
-   --values install/kubernetes/helm/istio/values-istio-auth.yaml \
-   install/kubernetes/helm/istio > install/kubernetes/istio-auth.yaml
-cat  install/kubernetes/istio-auth.yaml
+  # Check https://github.com/istio/test-infra/blob/master/boskos/configs.yaml
+  # for existing resources types
+  RESOURCE_TYPE="${RESOURCE_TYPE:-gke-perf-preset}"
+  OWNER='e2e-daily'
+  INFO_PATH="$(mktemp /tmp/XXXXX.boskos.info)"
+  FILE_LOG="$(mktemp /tmp/XXXXX.boskos.log)"
 
 
+  ISTIO_SHA=`curl $ISTIO_REL_URL/manifest.xml | grep -E "name=\"(([a-z]|-)*)/istio\"" | cut -f 6 -d \"`
+  [[ -z "${ISTIO_SHA}"  ]] && echo "error need to test with specific SHA" && exit 1
 
-export ISTIOCTL="${GOPATH}/src/istio.io/istio/${DAILY_BUILD}/bin/istioctl"
+  # Checkout istio at the greenbuild
+  mkdir -p ${GOPATH}/src/istio.io
+  pushd ${GOPATH}/src/istio.io
+# git clone https://github.com/istio/istio.git --depth 1 #KPTD
+  git clone https://github.com/morvencao/istio.git --depth 1 -b br_perf_istio_test
+  pushd istio
+# git checkout $ISTIO_SHA #KPTD
 
-get_resource "${RESOURCE_TYPE}" "${OWNER}" "${INFO_PATH}" "${FILE_LOG}"
-setup_cluster
+  # use uploaded yaml artifacts rather than the ones generated locally
+  DAILY_BUILD=istio-$(echo ${ISTIO_REL_URL} | cut -d '/' -f 6)
+  LINUX_DIST_URL=${ISTIO_REL_URL}/${DAILY_BUILD}-linux.tar.gz
+  DEB_URL=${ISTIO_REL_URL}/deb
+  #disable  ISTIO_REL_URL
+  unset     ISTIO_REL_URL
+
+  wget -q $LINUX_DIST_URL
+  tar -xzf ${DAILY_BUILD}-linux.tar.gz
+  cp    -R ${DAILY_BUILD}/install/* install/
+  cp install/kubernetes/istio-demo-auth.yaml install/kubernetes/istio-auth.yaml
+}
 
 # istio-boskos-perf-03:
 #   clusters:
@@ -109,29 +94,12 @@ function export_perf_variables() {
   export ZONE=`        grep  zone $PROJECT_INFO -m 1 | cut -f 2 -d : | sed 's/ //g' `
   [[ -z "${VM_NAME}"  ]] && echo "error could not parse vm name" && exit 13
 
-  export TOOLS_DIR="${PWD}/tools"
+  unset TOOLS_DIR
+  export ISTIOCTL="${GOPATH}/src/istio.io/istio/${DAILY_BUILD}/bin/istioctl"
 
-  ISTIOCTL=${ISTIOCTL:-bin/istioctl}
-  export ISTIOCTL
-
-  echo $PROJECT $CLUSTER_NAME $VM_NAME $ZONE $ISTIOCTL $TOOLS_DIR
+  echo ###############################################
+  echo $PROJECT $CLUSTER_NAME $VM_NAME $ZONE $ISTIOCTL
 }
-
-unset IFS
-export_perf_variables $INFO_PATH
-QPS=-1
-source "tools/setup_perf_cluster.sh"
-update_gcp_opts
-kubectl_setup
-
-NAMESP_FILE="$(mktemp /tmp/XXXXX.namespace.yaml)"
-cat > $NAMESP_FILE <<- EOM
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: istio-system
-EOM
-kubectl create -f $NAMESP_FILE
 
 function wait_istio_up() {
   for namespace in $(kubectl get namespaces --no-headers -o name); do
@@ -142,26 +110,14 @@ function wait_istio_up() {
 }
 
 function test_pods_up() {
-  not_running=`kubectl get pods --all-namespaces --no-headers | grep -v ^kube-system | grep -v "Running   0 " | wc -l`
+  not_running=`kubectl get pods --all-namespaces --no-headers | grep -v ^kube-system | grep -v "Running" | wc -l`
   if [[ "${not_running}" != "0" ]]; then
      kubectl get pods --all-namespaces | grep -v ^kube-system
      exit 4
   fi
 }
 
-function setup_istio_all_daily_override() {
-  update_gcp_opts
-  install_istio
-  install_istio_svc
-  wait_istio_up #wait
-  install_istio_ingress_rules
-  install_istio_cache_busting_rule
-  install_istio_addons
-  wait_istio_up #wait
-  setup_istio_addons_ingress
-}
-
-function install_perf_and_test() {
+function daily_install_setup_perf() {
   # setup VM
   update_gcp_opts
   setup_vm
@@ -173,10 +129,13 @@ function install_perf_and_test() {
   kubectl_setup
   install_non_istio_svc
   setup_non_istio_ingress
-#  setup_istio_all TODO
-  setup_istio_all_daily_override
+  setup_istio_all
 
   get_ips
+  echo VM_IP $VM_IP VM_URL $VM_URL
+}
+
+function daily_run_perf_test() {
   VERSION="" # reset in case it changed
   TS="" # reset once per set
   QPS=-1
@@ -186,9 +145,51 @@ function install_perf_and_test() {
   run_4_tests
 }
 
-PERF_LOG="$(mktemp /tmp/XXXXX.perf.log)" 
-install_perf_and_test -s 2>&1 > $PERF_LOG
-test_pods_up
+function daily_summarize() {
+  gsutil -m cp qps* gs://fortio-data/daily.releases/data/
+  echo
+}
 
-pwd;ls
-gsutil -m cp qps* gs://fortio-data/daily.releases/data/
+function create_istio_system_namespace() {
+  NAMESP_FILE="$(mktemp /tmp/XXXXX.namespace.yaml)"
+  cat > $NAMESP_FILE <<- EOM
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: istio-system
+EOM
+  kubectl create -f $NAMESP_FILE || echo istio-system most likely created
+}
+
+get_istio_code
+# directory now is istio root dir
+
+source "prow/mason_lib.sh"
+source "prow/cluster_lib.sh"
+# get boskos/mason resource
+get_resource "${RESOURCE_TYPE}" "${OWNER}" "${INFO_PATH}" "${FILE_LOG}"
+setup_cluster # setup boskos cluster
+trap cleanup EXIT
+
+
+echo core account is
+gcloud config get-value core/account
+
+unset IFS
+export_perf_variables $INFO_PATH
+QPS=-1
+source "tools/setup_perf_cluster.sh"
+update_gcp_opts
+kubectl_setup
+create_istio_system_namespace
+
+echo ======================================= set up perf env ================================================
+daily_install_setup_perf
+echo ======================================= run perf test ==================================================
+daily_run_perf_test
+echo ======================================= dailyperf test done ==========================================
+
+test_pods_up
+# summarize the information fail/pass, and copy if pass???
+daily_summarize
+
