984a6ac56a753c48ee26781e70ae7cced5fc7d5e
Istio Release Robot Account <34667608+istio-release-robot@users.noreply.github.com>
2018-11-26 17:50:05 -0800

    relQual_1543282381815292756 (#1292)

--- test/build_env.sh
+++ test/build_env.sh
@@ -1,13 +1,14 @@
 # This script holds parameters specific to an instance of a build
-export CB_BRANCH=master
+#fake 2
+export CB_BRANCH=krishna-test
 export CB_CHECK_GREEN_SHA_AGE=true
 export CB_COMMIT=
 export CB_DOCKER_HUB=gcr.io/istio-release
 export CB_GCS_BUILD_BUCKET=istio-release-pipeline-data
-export CB_GCS_BUILD_PATH=istio-release-pipeline-data/daily-build/krishna-test2
-export CB_GCS_FULL_STAGING_PATH=istio-prerelease/daily-build/krishna-test2
+export CB_GCS_BUILD_PATH=istio-release-pipeline-data/daily-build/krishna-test-20181127-00-01
+export CB_GCS_FULL_STAGING_PATH=istio-prerelease/daily-build/krishna-test-20181127-00-01
 export CB_GCS_MONTHLY_RELEASE_PATH=
-export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/daily-build/krishna-test2
+export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/daily-build/krishna-test-20181127-00-01
 export CB_GCS_STAGING_BUCKET=istio-prerelease
 export CB_GITHUB_ORG=istio
 export CB_GITHUB_TOKEN_FILE_PATH=istio-secrets/github.txt.enc
@@ -16,4 +17,4 @@ export CB_PIPELINE_TYPE=daily
 export CB_PUSH_DOCKER_HUBS=gcr.io/istio-release
 export CB_TEST_GITHUB_TOKEN_FILE_PATH=
 export CB_VERIFY_CONSISTENCY=false
-export CB_VERSION=krishna-test2
+export CB_VERSION=krishna-test-20181127-00-01
