fd191d82606048a4e526a7b85e8e845cbfe54aba
Andy Lai <hklai@google.com>
2018-12-13 09:41:02 -0800

    Triger new build

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,13 +1,13 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=master
 export CB_CHECK_GREEN_SHA_AGE=true
-export CB_COMMIT=37a92ed26f7c1c8326e927fa0bd9251cdec15277
+export CB_COMMIT=913b459130045fc4846a36c46c05a48da88776bb
 export CB_DOCKER_HUB=gcr.io/istio-release
 export CB_GCS_BUILD_BUCKET=istio-release-pipeline-data
-export CB_GCS_BUILD_PATH=istio-release-pipeline-data/daily-build/master-20181212-23-41
-export CB_GCS_FULL_STAGING_PATH=istio-prerelease/daily-build/master-20181212-23-41
+export CB_GCS_BUILD_PATH=istio-release-pipeline-data/daily-build/master-20181213-2040
+export CB_GCS_FULL_STAGING_PATH=istio-prerelease/daily-build/master-20181213-2040
 export CB_GCS_MONTHLY_RELEASE_PATH=
-export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/daily-build/master-20181212-23-41
+export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/daily-build/master-20181213-2040
 export CB_GCS_STAGING_BUCKET=istio-prerelease
 export CB_GITHUB_ORG=istio
 export CB_GITHUB_TOKEN_FILE_PATH=istio-secrets/github.txt.enc
@@ -16,5 +16,5 @@ export CB_PIPELINE_TYPE=daily
 export CB_PUSH_DOCKER_HUBS=gcr.io/istio-release
 export CB_TEST_GITHUB_TOKEN_FILE_PATH=
 export CB_VERIFY_CONSISTENCY=false
-export CB_VERSION=master-20181212-23-41
+export CB_VERSION=master-20181213-2040
 
