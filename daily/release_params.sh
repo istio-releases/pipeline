3dca269b942734e83022984f3b4e915caf8be99d
Andy Lai <hklai@google.com>
2018-12-14 15:33:05 -0800

    Trying a new build

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,5 +1,5 @@
 export CB_BRANCH=master
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=master-20181214-05-47
+export CB_VERSION=master-20181215-03-32
 export CB_COMMIT=913b459130045fc4846a36c46c05a48da88776bb
 
