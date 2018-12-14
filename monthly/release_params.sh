e47a8e3f3f61be5192057657a23f733e70006df4
Andy Lai <hklai@google.com>
2018-12-14 15:27:28 -0800

    Simplify param files and remove junk

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,19 +1,4 @@
-# This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_CHECK_GREEN_SHA_AGE=true
 export CB_COMMIT=f927d1ec433cecc6f66fcdcc0af38327b70efa68
-export CB_DOCKER_HUB=docker.io/istio
-export CB_GCS_BUILD_BUCKET=istio-release-pipeline-data
-export CB_GCS_BUILD_PATH=istio-release-pipeline-data/prerelease/1.1.0-snapshot.4
-export CB_GCS_FULL_STAGING_PATH=istio-prerelease/prerelease/1.1.0-snapshot.4
-export CB_GCS_MONTHLY_RELEASE_PATH=istio-release/releases/1.1.0-snapshot.4
-export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/prerelease/1.1.0-snapshot.4
-export CB_GCS_STAGING_BUCKET=istio-prerelease
-export CB_GITHUB_ORG=rkpagadala
-export CB_GITHUB_TOKEN_FILE_PATH=istio-secrets/github.txt.enc
-export CB_ISTIOCTL_DOCKER_HUB=docker.io/istio
 export CB_PIPELINE_TYPE=monthly
-export CB_PUSH_DOCKER_HUBS=docker.io/istio
-export CB_TEST_GITHUB_TOKEN_FILE_PATH=
-export CB_VERIFY_CONSISTENCY=false
 export CB_VERSION=1.1.0-snapshot.4
