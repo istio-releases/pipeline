1cceff39049843196742405fe76f3bc598d90e42
Andy Lai <hklai@google.com>
2018-12-14 15:27:28 -0800

    Simplify param files and remove junk

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,20 +1,5 @@
-# This script holds parameters specific to an instance of a build
 export CB_BRANCH=master
-export CB_CHECK_GREEN_SHA_AGE=true
-export CB_COMMIT=913b459130045fc4846a36c46c05a48da88776bb
-export CB_DOCKER_HUB=gcr.io/istio-release
-export CB_GCS_BUILD_BUCKET=istio-release-pipeline-data
-export CB_GCS_BUILD_PATH=istio-release-pipeline-data/daily-build/master-20181213-20-40
-export CB_GCS_FULL_STAGING_PATH=istio-prerelease/daily-build/master-20181213-20-40
-export CB_GCS_MONTHLY_RELEASE_PATH=
-export CB_GCS_RELEASE_TOOLS_PATH=istio-release-pipeline-data/release-tools/daily-build/master-20181213-20-40
-export CB_GCS_STAGING_BUCKET=istio-prerelease
-export CB_GITHUB_ORG=istio
-export CB_GITHUB_TOKEN_FILE_PATH=istio-secrets/github.txt.enc
-export CB_ISTIOCTL_DOCKER_HUB=gcr.io/istio-release
 export CB_PIPELINE_TYPE=daily
-export CB_PUSH_DOCKER_HUBS=gcr.io/istio-release
-export CB_TEST_GITHUB_TOKEN_FILE_PATH=
-export CB_VERIFY_CONSISTENCY=false
-export CB_VERSION=master-20181213-20-40
+export CB_VERSION=master-20181214-05-47
+export CB_COMMIT=913b459130045fc4846a36c46c05a48da88776bb
 
