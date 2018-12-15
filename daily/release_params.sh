720a656f749d87fbabf48734e483ecf5188b533b
Andy Lai <hklai@google.com>
2018-12-14 17:10:25 -0800

    Fix

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.0
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=master-20181215-05-00
+export CB_VERSION=release-1.0-20181215-05-00
 export CB_COMMIT=fb6c64c94eb6e4d81a4e1a28ec1f299652b0037a
