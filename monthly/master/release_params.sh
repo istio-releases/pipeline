1e68d078e9f28b6c913fece0bbf6a62d7accd759
hklai <hklai@google.com>
2018-12-17 14:33:48 -0800

    Adding branch specific param files

--- monthly/master/release_params.sh
+++ monthly/master/release_params.sh
@@ -1,7 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=master
 export CB_COMMIT=f927d1ec433cecc6f66fcdcc0af38327b70efa68
-export CB_GITHUB_ORG=istio
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.0-snapshot.4
+export CB_VERSION=1.2.0-snapshot.0
 
---
+++ monthly/release-1.1/release_params.sh
@@ -0,0 +1,6 @@
+# This script holds parameters specific to an instance of a build
+export CB_BRANCH=release-1.1
+export CB_COMMIT=f927d1ec433cecc6f66fcdcc0af38327b70efa68
+export CB_PIPELINE_TYPE=monthly
+export CB_VERSION=1.1.0-snapshot.4
+
