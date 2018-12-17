dea59d8612f3f0f3c08280f93ac0a82c2bf14ed9
TestRunnerBot <testrunner@istio.io>
2018-12-17 09:16:08 +0000

    release_1545038168575146597

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20181216-09-15
-export CB_COMMIT=ef138e81e125efad2059570c78c8e5555861ce4d
+export CB_VERSION=release-1.1-20181217-09-15
+export CB_COMMIT=d3f07060107b3dbb8c3331bcaaf0403c826c020e
