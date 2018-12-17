b87ba16f6605f545fd19b1e67617f292409ce92b
hklai <hklai@google.com>
2018-12-17 14:53:02 -0800

    release_1545087181602983921

--- daily/release-1.0/release_params.sh
+++ daily/release-1.0/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.0
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.0-20181217-16-15
+export CB_VERSION=release-1.0-20181217-14-52
 export CB_COMMIT=fb6c64c94eb6e4d81a4e1a28ec1f299652b0037a
