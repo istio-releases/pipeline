c8f111cbe9c32cd4bd02f3c24ed838a2e8c5ee5f
hklai <hklai@google.com>
2018-12-17 18:34:06 -0800

    MONTHLY 1.1.0-snapshot.4

--- monthly/release-1.1/release_params.sh
+++ monthly/release-1.1/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=f927d1ec433cecc6f66fcdcc0af38327b70efa68
+export CB_COMMIT=e661de08e04e78e60f7fbec067e4357974fa27c3
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-snapshot.4
 
