10ecc362896703deb59e53bf6498d5c8cddd89a9
hklai <hklai@google.com>
2018-12-17 18:47:08 -0800

    Disable consistency check for snapshot release

--- monthly/release-1.1/release_params.sh
+++ monthly/release-1.1/release_params.sh
@@ -3,4 +3,5 @@ export CB_BRANCH=release-1.1
 export CB_COMMIT=e661de08e04e78e60f7fbec067e4357974fa27c3
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-snapshot.4
+export CB_VERIFY_CONSISTENCY=false
 
