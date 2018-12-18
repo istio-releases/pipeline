72446b7588a5403577ee4c7a8874bed0f4aac230
Istio Release Robot Account <34667608+istio-release-robot@users.noreply.github.com>
2018-12-18 02:06:14 -0800

    release_1545124571082226558 (#105)
    Testing

--- daily/master/release_params.sh
+++ daily/master/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=master
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=master-20181217-15-35
+export CB_VERSION=master-20181218-09-15
 export CB_COMMIT=913b459130045fc4846a36c46c05a48da88776bb
