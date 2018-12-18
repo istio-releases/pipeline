9addbcd1f44ae50f8621d7bb1cd6dc2472089590
hklai <hklai@google.com>
2018-12-18 11:53:47 -0800

    Restore release branches in istio-releases/pipeline

--- monthly/release-1.0/release_params.sh
+++
@@ -1,6 +0,0 @@
-# This script holds parameters specific to an instance of a build
-export CB_BRANCH=release-1.0
-export CB_COMMIT=fb6c64c94eb6e4d81a4e1a28ec1f299652b0037a
-export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.0.5
-
