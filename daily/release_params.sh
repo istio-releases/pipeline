8d0e611f083fe4f619cb68519e2202fed598bdb7
Pengyuan Bian <bianpengyuan@google.com>
2018-12-19 13:45:07 -0800

    release_1545253101071432000 (#114)
    Release qualification passed

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20181219-10-12
-export CB_COMMIT=a117ac46fe9c69d59cc11fc6839c13ffb6105442
+export CB_VERSION=release-1.1-20181219-12-58
+export CB_COMMIT=b498c245b534d0a3f549895dab5f313d95257d5e
