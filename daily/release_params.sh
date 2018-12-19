c1bb726f35e0cede366f54e325c65f238aedfb29
Pengyuan Bian <bianpengyuan@google.com>
2018-12-19 11:10:08 -0800

    create a new daily release for 1.1 (#113)
    Release qualification passed

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20181219-09-15
+export CB_VERSION=release-1.1-20181219-10-12
 export CB_COMMIT=a117ac46fe9c69d59cc11fc6839c13ffb6105442
