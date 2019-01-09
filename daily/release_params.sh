4838f7f8e3e93a1b046e3cd5360e92bd48d630f3
Matt Greenfield <matt.greenfield@datadoghq.com>
2019-01-09 12:50:19 -0700

    Trigger daily build release-1.1-20190109-12-48
    Build commit 7ede847f89d6887c8300dd73faa31e99ced99d42

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20190109-09-15
-export CB_COMMIT=69d8588ccd743dd1f540d14a494e1b36b6e4506e
+export CB_VERSION=release-1.1-20190109-12-48
+export CB_COMMIT=7ede847f89d6887c8300dd73faa31e99ced99d42
