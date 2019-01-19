663b529b1930b822961003b3328086f40b6f767b
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2019-01-18 20:51:19 -0800

    Daily release-1.1-20190118-20-49

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
-export CB_BRANCH=master
+export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=master-20190118-09-15
-export CB_COMMIT=b8e30e07ec7b36cf1ba1b445b78c81fdf0d417b9
+export CB_VERSION=release-1.1-20190118-20-49
+export CB_COMMIT=b0a8dedafbeb02ff5d6fd9f0c3973ffc4b084dee
