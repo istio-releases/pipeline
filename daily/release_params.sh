01d9e586e093ae5bbc878ae762c18f93c62078a1
Istio Release Robot Account <34667608+istio-release-robot@users.noreply.github.com>
2019-01-19 02:22:42 -0800

    release_1547889410803917971 (#218)
    Release qualification passed

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20190118-09-15
-export CB_COMMIT=75e315adc64c102055f6001666fba6940ef89804
+export CB_VERSION=release-1.1-20190119-09-16
+export CB_COMMIT=b0a8dedafbeb02ff5d6fd9f0c3973ffc4b084dee
