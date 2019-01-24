55bacb0691fcfc8b857cccab439ffdff6b91d9a9
Andy Lai <31747472+hklai@users.noreply.github.com>
2019-01-24 11:32:43 -0800

    release_1548352953863794412 (#238)
    Release qualification passed

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20190124-09-17
-export CB_COMMIT=fc03e9f2a8f99ebb04cd4c0366edae8a9b9cc9b4
+export CB_VERSION=release-1.1-20190124-10-02
+export CB_COMMIT=c7eb603ee0b1534382499a2e4378a8b4348fdb07
