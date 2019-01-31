c0f7353276196af4922c49bce481282904527d51
hklai <hklai@google.com>
2019-01-31 12:58:35 -0800

    Use a newer SHA

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=f1eab0a25dd6f37be03951698be0e84b120ec18f
+export CB_COMMIT=d43b689df3f879ee166e63afb5ad6c4fbb7fc738
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-snapshot.test
 export CB_VERIFY_CONSISTENCY=false
