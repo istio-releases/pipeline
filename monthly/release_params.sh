96e675c877afe965c55263b5fcb86bf64e7d505a
hklai <hklai@google.com>
2019-01-31 11:50:30 -0800

    Test snapshot

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,6 +2,6 @@
 export CB_BRANCH=release-1.1
 export CB_COMMIT=f1eab0a25dd6f37be03951698be0e84b120ec18f
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.0-snapshot.5
+export CB_VERSION=1.1.0-snapshot.test
 export CB_VERIFY_CONSISTENCY=false
 
