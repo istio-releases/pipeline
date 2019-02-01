52f6eaf9372f57296a65a4023b0ea8e4e5fecae7
hklai <hklai@google.com>
2019-02-01 10:22:56 -0800

    Release 1.0.6

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.0
-export CB_COMMIT=fb6c64c94eb6e4d81a4e1a28ec1f299652b0037a
+export CB_COMMIT=9dccd5a94b50f97d0b39aebf3a4ed4dc50c94b58
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.0.5
+export CB_VERSION=1.0.6
 
