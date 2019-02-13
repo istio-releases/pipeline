dbd8451fa56eb96ec60cbf673e7ddbc1579c495c
Joshua Blatt <jblatt@google.com>
2019-02-13 09:30:25 -0800

    Update to istio/istio release-1.1 HEAD

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=a5237c43e572c781fbe97aa1af0d2b316908dd7b
+export CB_COMMIT=14777199b85ba56ebca2a4516afff33c97199eb4
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-snapshot.6
 export CB_VERIFY_CONSISTENCY=true
