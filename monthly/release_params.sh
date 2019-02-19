00b3bee6b69eed3b1e6333c378be59811dde53f8
Maria Skidanova <skidam@google.com>
2019-02-19 12:23:36 -0800

    Creating rc0

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,7 +1,7 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=14777199b85ba56ebca2a4516afff33c97199eb4
+export CB_COMMIT=76aa6028df2bb3529e9e5ad054cbb6522338fc3c
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.0-snapshot.6
+export CB_VERSION=1.1.0-rc.0
 export CB_VERIFY_CONSISTENCY=true
 
