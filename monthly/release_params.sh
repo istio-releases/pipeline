886b1d769bd858d6e119e38bccdf2b9b627ddd6f
Maria Skidanova <skidam@google.com>
2019-02-19 19:49:51 -0800

    New sha

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=44c6b5fc56a8f0e7ecee7c3b4d76ce8df9493437
+export CB_COMMIT=3632361b439ae27962f0af472064d452fc63273b
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-rc.0
 export CB_VERIFY_CONSISTENCY=true
