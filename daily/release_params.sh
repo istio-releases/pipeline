620f3acd6d6e176c393e94511e18df636dd38261
Maria Skidanova <skidam@google.com>
2019-02-20 11:46:17 -0800

    Fix daily for 1.1

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20190214-09-16
-export CB_COMMIT=6113e155ac85e2485e30dfea2b80fd97afd3130a
+export CB_VERSION=release-1.1-20190220-11-45
+export CB_COMMIT=8ba17af0d9036ccda21790c598e813a4ba397090
