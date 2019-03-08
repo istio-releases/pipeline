e319dcee094167582ab278cd061099fc23252415
Maria Skidanova <skidam@google.com>
2019-03-08 09:22:57 -0800

    rc3

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,7 +1,7 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=a5e38d2cbedbb1eeb8f60b24176656d1813dea44
+export CB_COMMIT=653f0050f57c7bfbc238e8a92cce1f0eb6896863
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.0-rc.2
+export CB_VERSION=1.1.0-rc.3
 export CB_VERIFY_CONSISTENCY=true
 
