7e4a83ede802039445ab37d7be4acf4e54108492
Joshua Blatt <jblatt@google.com>
2019-03-12 13:34:46 -0700

    Sync istio->api and istio->proxy->api dependencies.

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=bd3554646adde443dc95aeb775c831a80c5827cf
+export CB_COMMIT=d23daa3d242e51b8fff513f3ac86a708e9bcceba
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0-rc.4
 export CB_VERIFY_CONSISTENCY=true
