5460b3707049f5100caa936e353adc4b04219871
Joshua Blatt <jblatt@google.com>
2019-03-16 11:29:48 -0700

    1.1 release (#418)
    Release qualification passed

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,6 +2,6 @@
 export CB_BRANCH=release-1.1
 export CB_COMMIT=82797c0c0649a3f73029b33957ae105260458c6e
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.0-rc.6
+export CB_VERSION=1.1.0
 export CB_VERIFY_CONSISTENCY=true
 
