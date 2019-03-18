90201d6d57c347d21ade6cf891e82593a0017316
Joshua Blatt <jblatt@google.com>
2019-03-18 09:56:27 -0700

    Use a different sha from RC6 to force the pipeline to create a new release.

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=82797c0c0649a3f73029b33957ae105260458c6e
+export CB_COMMIT=190d4c1c59328e2aca2611f3056a03d45c583ca8
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.0
 export CB_VERIFY_CONSISTENCY=true
