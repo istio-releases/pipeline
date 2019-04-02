0e466e8bdb8afe102b2faedc22c8769dedb295ae
Maria Skidanova <skidam@google.com>
2019-04-02 10:40:04 -0700

    Create 1.1.2

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,6 +2,6 @@
 export CB_BRANCH=release-1.1
 export CB_COMMIT=2b1331886076df103179e3da5dc9077fed59c989
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.1
+export CB_VERSION=1.1.2
 export CB_VERIFY_CONSISTENCY=true
 
