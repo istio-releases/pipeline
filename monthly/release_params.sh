804b5a5ba9ffaf180027ade5586091d4cb7d90a4
Maria Skidanova <skidanova.maria@gmail.com>
2019-04-02 11:39:48 -0700

    Creating 1.0.7 (#478)
    Release qualification passed

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,5 +2,6 @@
 export CB_BRANCH=release-1.0
 export CB_COMMIT=98598f88f6ee9c1e6b3f03b652d8e0e3cd114fa2
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.0.6
+export CB_VERSION=1.0.7
+export CB_CHECK_GREEN_SHA_AGE=false
 
