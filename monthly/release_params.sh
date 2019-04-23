aa2bffebedeffaf54bd34a1aaf4162669d6d8b2d
Maria Skidanova <skidam@google.com>
2019-04-23 16:14:10 -0700

    Creating release 1.4

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,7 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=d19179769183541c5db473ae8d062ca899abb3be
+export CB_COMMIT=1cc5778b17a3c949db11951395cbf2a168e30696
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.3
+export CB_VERSION=1.1.4
 export CB_VERIFY_CONSISTENCY=true
-export CB_CHECK_GREEN_SHA_AGE=false
