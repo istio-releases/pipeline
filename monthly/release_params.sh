7b7820cda94a12c423a35ecbe3d3a169e3f1efba
Maria Skidanova <skidam@google.com>
2019-04-24 11:41:49 -0700

    Update istio sha for 1.1.4 (#553)
    Release qualification passed

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=1cc5778b17a3c949db11951395cbf2a168e30696
+export CB_COMMIT=bc7465793cbff4c4189639b3f404e21c517cbdc6
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.4
 export CB_VERIFY_CONSISTENCY=true
