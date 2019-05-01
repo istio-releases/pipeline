ed2be0c3039b4cf1aceaeb144bf231f0383df2ff
Joshua Blatt <jblatt@google.com>
2019-05-01 12:21:49 -0700

    Release 1.1.5 (#572)
    Release qualification passed

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=bc7465793cbff4c4189639b3f404e21c517cbdc6
+export CB_COMMIT=9b6d31b74d1c0cc9358cc82d395b53f71393326b
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.4
+export CB_VERSION=1.1.5
 export CB_VERIFY_CONSISTENCY=true
