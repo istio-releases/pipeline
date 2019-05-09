853c733af8750c4c5ed2e472552ec07d1b22f7b0
lei-tang <32078630+lei-tang@users.noreply.github.com>
2019-05-09 16:10:50 -0700

    Create a new build from the latest code with hash c9bcad4 (#598)
    Release qualification passed

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.1
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.1-20190509-12-00
-export CB_COMMIT=3ca97810559834761575cc1a69cfc5cea695d300
+export CB_VERSION=release-1.1-20190509-14-54
+export CB_COMMIT=c9bcad4b474b3c0ec2035b6bf91782f55f785987
