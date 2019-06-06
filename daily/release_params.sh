7be9cdd11bc3c6b28d3aed1f24cda4ec3dfc396e
Joshua Blatt <jblatt@google.com>
2019-06-05 17:20:45 -0700

    Trigger daily build to facilitate testing for 1.0.8

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.0
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.0-20190208-09-16
-export CB_COMMIT=98598f88f6ee9c1e6b3f03b652d8e0e3cd114fa2
+export CB_VERSION=release-1.0-20190605-17-17
+export CB_COMMIT=11b640bb11593138a904f81e572d40e5e70b089b
