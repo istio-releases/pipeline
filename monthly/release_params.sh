32c2cafeb008bf114da31719dd97b4bb8daf761d
Joshua Blatt <jblatt@google.com>
2019-06-14 09:36:03 -0700

    Remove errant character in git SHA.

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=591173c26c635c6f977964303649a5273deb9687:
+export CB_COMMIT=591173c26c635c6f977964303649a5273deb9687
 export CB_PIPELINE_TYPE=monthly
 export CB_VERSION=1.1.9
 export CB_VERIFY_CONSISTENCY=true
