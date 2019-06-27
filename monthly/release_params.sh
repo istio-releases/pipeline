3287528ceb57c4e209ace8bc0b1b7200f0649d83
Francois Pesce <fpesce@google.com>
2019-06-27 09:37:52 -0700

    Build release 1.2.2

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.2
-export CB_COMMIT=bbe7afd4d157ac105e84b6b9fc9cb695f188c684
+export CB_COMMIT=cd4a148f37dc386986d6a6d899778849e686beea
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.2.1
+export CB_VERSION=1.2.2
 export CB_VERIFY_CONSISTENCY=true
