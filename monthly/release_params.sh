824dfb7d2738c656b1691798c44c7e02d1f80050
Joshua Blatt <jblatt@google.com>
2019-06-26 21:20:58 -0700

    Build 1.1.10 release

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,6 +1,6 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.1
-export CB_COMMIT=879413c25b871ae6fd0df82bea1605baf12cba2e
+export CB_COMMIT=b5f85e976c1197a4a56848a783cc6df57be3907f
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.1.9
+export CB_VERSION=1.1.10
 export CB_VERIFY_CONSISTENCY=true
