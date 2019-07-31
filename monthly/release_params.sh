41a3abb985d24371b4e0f33b1a78d866abc0355a
Francois Pesce <fpesce@google.com>
2019-07-31 16:40:42 -0700

    Trigger pipeline with non-empty commit

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,4 +1,5 @@
 # This script holds parameters specific to an instance of a build
+
 export CB_BRANCH=release-1.2
 export CB_COMMIT=c562694ea6e554c2b60d12c9876d2641cfdd917d
 export CB_PIPELINE_TYPE=monthly
