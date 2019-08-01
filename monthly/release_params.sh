9f8347a4152d1de7390b99109a67555b64d9dcf0
Francois Pesce <fpesce@google.com>
2019-07-31 18:29:23 -0700

    Non-empty commit (#971)
    Release qualification passed

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,4 +1,5 @@
 # This script holds parameters specific to an instance of a build
+
 export CB_BRANCH=release-1.1
 export CB_COMMIT=473d9b376acf780fa1929eaf44dc65a3049d3765
 export CB_PIPELINE_TYPE=monthly
