64178b5bf1819177f8361808630359a8ac09f026
Francois Pesce <fpesce@google.com>
2019-08-07 14:11:36 -0700

    Build 1.2.4 with no changes from 1.2.3 (#1002)

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -3,5 +3,6 @@
 export CB_BRANCH=release-1.2
 export CB_COMMIT=c562694ea6e554c2b60d12c9876d2641cfdd917d
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.2.3
+export CB_VERSION=1.2.4
 export CB_VERIFY_CONSISTENCY=true
+export CB_CHECK_GREEN_SHA_AGE=false
