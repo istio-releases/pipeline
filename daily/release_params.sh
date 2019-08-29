48642c458f49b2e0e13d7e5bbb27e65d8f6f4fa0
John Howard <howardjohn@google.com>
2019-08-29 10:47:32 -0700

    Manual daily build (#1114)
    Pinned to the operator update SHA-1

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.3
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.3-20190828-09-16
-export CB_COMMIT=19f57d8ad2890602f6f645f82cb3273eef013b47
+export CB_VERSION=release-1.3-20190829-10-16
+export CB_COMMIT=71170c6b4ac37d1d7b312add07446ced1977d1d5
