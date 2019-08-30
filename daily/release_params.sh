e07b35b1784b844979440be54318fa3b2d651379
John Howard <howardjohn@google.com>
2019-08-30 16:38:29 -0700

    Manual build (#1121)

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.3
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.3-20190829-10-16
-export CB_COMMIT=71170c6b4ac37d1d7b312add07446ced1977d1d5
+export CB_VERSION=release-1.3-20190830-10-16
+export CB_COMMIT=7b91f42b683ff232c0ac1b61856f9dcd697ed56b
