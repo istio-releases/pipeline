f283ae4b2385add751b7af7f50146cbde9f43fc3
Romain Lenglet <romain.lenglet@berabera.info>
2019-09-06 19:38:43 -0700

    Build release 1.3.0-rc.2 (#1158)

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,7 +1,7 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.3
-export CB_COMMIT=ba6de8d47d8d34333c605e1b0222dedb4f9840a6
+export CB_COMMIT=8710d000681ecc3dcf368b7cffc7d9415cab90ab
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.3.0-rc.1
+export CB_VERSION=1.3.0-rc.2
 export CB_VERIFY_CONSISTENCY=true
 export CB_CHECK_GREEN_SHA_AGE=false
