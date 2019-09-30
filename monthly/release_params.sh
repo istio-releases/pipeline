04567f8c11485ada6b4507746c47fa24d14fd56b
Francois Pesce <fpesce@google.com>
2019-09-30 09:09:43 -0700

    Trigger a build on current release-1.2 of istio
    NB: some artefacts of this build might be manually replaced on 10/8

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,8 +1,8 @@
 # This script holds parameters specific to an instance of a build
 
 export CB_BRANCH=release-1.2
-export CB_COMMIT=b26019f05195188378be9118b0f0fa7613f7ebb4
+export CB_COMMIT=323094605c9c4afead13b46eb8da7d96673d9c6e
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.2.6
+export CB_VERSION=1.2.7
 export CB_VERIFY_CONSISTENCY=true
 export CB_CHECK_GREEN_SHA_AGE=false
