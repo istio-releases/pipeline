e3a40c60ff58f1c51e8ee309ad5e293a0310de50
Romain Lenglet <romain.lenglet@berabera.info>
2019-10-01 10:57:25 -0700

    Build release 1.3.2 (#1279)

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,6 +2,6 @@
 export CB_BRANCH=release-1.3
 export CB_COMMIT=12c4d01552363362b49fce86f83b847b308b9b0d
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.3.1
+export CB_VERSION=1.3.2
 export CB_VERIFY_CONSISTENCY=true
 export CB_CHECK_GREEN_SHA_AGE=false
