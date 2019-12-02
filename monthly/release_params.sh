023bf48c1c94b649b8cee2d54ffefbb5b77df534
Romain Lenglet <rlenglet@google.com>
2019-12-02 15:41:16 -0800

    Build release 1.3.6

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -1,7 +1,7 @@
 # This script holds parameters specific to an instance of a build
 export CB_BRANCH=release-1.3
-export CB_COMMIT=9c4f3fa302ac85a70ec5b03049c6a66a5b626f7a
+export CB_COMMIT=4b745189e0e4c8d735216cb26d821eb201a5a89d
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.3.5
+export CB_VERSION=1.3.6
 export CB_VERIFY_CONSISTENCY=true
 export CB_CHECK_GREEN_SHA_AGE=false
