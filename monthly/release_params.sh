308a8bd8636acdde147956f33d7f6ee72f0609a9
Romain Lenglet <romain.lenglet@berabera.info>
2020-02-05 00:50:52 -0800

    Build release 1.3.8 (#1536)

--- monthly/release_params.sh
+++ monthly/release_params.sh
@@ -2,6 +2,6 @@
 export CB_BRANCH=release-1.3
 export CB_COMMIT=23077869571812df7e888efdf1fb00c749f1cfcf
 export CB_PIPELINE_TYPE=monthly
-export CB_VERSION=1.3.7
+export CB_VERSION=1.3.8
 export CB_VERIFY_CONSISTENCY=true
 export CB_CHECK_GREEN_SHA_AGE=false
