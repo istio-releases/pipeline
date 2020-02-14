e74fce0eeb2496262b5b288ec3ee812615ea6c13
Istio Release Robot <34667608+istio-release-robot@users.noreply.github.com>
2020-02-14 04:43:46 -0800

    release_1581675362306982022 (#1546)

--- daily/release_params.sh
+++ daily/release_params.sh
@@ -1,4 +1,4 @@
 export CB_BRANCH=release-1.3
 export CB_PIPELINE_TYPE=daily
-export CB_VERSION=release-1.3-20200213-10-16
+export CB_VERSION=release-1.3-20200214-10-15
 export CB_COMMIT=3db95dfc23ffc081803b42549934915ba3b0a3d5
